18051c0b667539c19eaf54c96fd5fbb0bdaaf0eb
yangkequn <yangkequn@gmail.com>
2022-09-02 10:42:49 +0800

    add Internel error

--- http/vars.go
+++ http/vars.go
@@ -2,6 +2,12 @@ package http
 
 import (
 	"errors"
+	. "net/http"
 )
 
 var ErrJWT error = errors.New("JWT error")
+
+func InternalError(w ResponseWriter, err error) {
+	w.WriteHeader(StatusInternalServerError)
+	w.Write([]byte(err.Error()))
+}
