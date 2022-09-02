84971872e92be48c5d731d8ddc3918245006e79c
yangkequn <yangkequn@gmail.com>
2022-09-02 10:42:11 +0800

    improve performence,fix jwt bug

--- http/jwt.go
+++ http/jwt.go
@@ -2,22 +2,38 @@ package http
 
 import (
 	"fmt"
-	"net/http"
-
 	. "saavuu/config"
 
 	"github.com/golang-jwt/jwt/v4"
 )
 
-func JwtFromHttpRequest(r *http.Request) (t *jwt.Token, err error) {
-	jwtStr := r.Header.Get("Authorization")
-	if jwtStr == "" {
-		return nil, fmt.Errorf("no Authorization header")
+func (svc *HttpContext) JwtField(field string) (f interface{}) {
+	if svc.jwtToken == nil {
+		jwtStr := svc.Req.Header.Get("Authorization")
+		if len(jwtStr) == 0 {
+			return nil
+		}
+		//decode jwt string to map[string] interface{} with jwtSrcrets as jwt secret
+		keyFunction := func(token *jwt.Token) (value interface{}, err error) {
+			_, ok := token.Method.(*jwt.SigningMethodHMAC)
+			if !ok {
+				return nil, fmt.Errorf("Invalid signing method")
+			}
+			return []byte(Cfg.JwtSecret), nil
+		}
+		svc.jwtToken, _ = jwt.ParseWithClaims(jwtStr, jwt.MapClaims{}, keyFunction)
+		if svc.jwtToken == nil {
+			return nil
+		}
+
 	}
-	//decode jwt string to map[string] interface{} with jwtSrcrets as jwt secret
-	//map[string] interface{} is the type of jwt.Claims
-	keyFunction := func(t *jwt.Token) (value interface{}, err error) {
-		return []byte(Cfg.JwtSecret), nil
+	// return field in svc.Jwt.Claims
+	mpclaims, ok := svc.jwtToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil
 	}
-	return jwt.ParseWithClaims(jwtStr, &jwt.MapClaims{}, keyFunction)
+	if f, ok = mpclaims[field]; !ok {
+		return nil
+	}
+	return f
 }
