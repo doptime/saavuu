fc28ff55dc40343be72543db77787ffb9011d45b
yangkequn <yangkequn@gmail.com>
2022-09-19 15:12:32 +0800

    refactor

--- config/readConfig.go
+++ config/readConfig.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"saavuu/redisService"
 	"strconv"
 
 	"github.com/go-redis/redis/v8"
@@ -15,7 +14,7 @@ func LoadConfigFromEnv() (err error) {
 	os.Setenv("REDIS_ADDRESS_PARAM", "docker.vm:6379")
 	os.Setenv("REDIS_PASSWORD_PARAM", "")
 	os.Setenv("REDIS_DB_PARAM", "0")
-	os.Setenv("REDIS_CONFIG_KEY", "config_saavuu_services")
+	os.Setenv("REDIS_CONFIG_KEY", "saavuu_service_config")
 	REDIS_ADDRESS_PARAM := os.Getenv("REDIS_ADDRESS_PARAM")
 	if len(REDIS_ADDRESS_PARAM) == 0 {
 		panic("env REDIS_ADDRESS_PARAM is not set")
@@ -42,22 +41,28 @@ func LoadConfigFromEnv() (err error) {
 		panic("env REDIS_CONFIG_KEY is not set")
 	}
 
-	rds := Cfg.ParamRedis
 	REDIS_ADDRESS_DATA := REDIS_ADDRESS_PARAM
-	if err = redisService.HGet(ctx, rds, ConfigKey, "REDIS_ADDRESS_DATA", &REDIS_ADDRESS_DATA); err != nil {
-		redisService.HSet(ctx, rds, ConfigKey, "REDIS_ADDRESS_DATA", REDIS_ADDRESS_PARAM)
+	if cmd := Cfg.ParamRedis.HGet(ctx, ConfigKey, "REDIS_ADDRESS_DATA"); cmd.Err() == nil {
+		REDIS_ADDRESS_DATA = cmd.Val()
+	} else if cmd.Err() != redis.Nil {
+		Cfg.ParamRedis.HSet(ctx, ConfigKey, "REDIS_ADDRESS_DATA", REDIS_ADDRESS_PARAM)
 		fmt.Println("REDIS_ADDRESS_DATA is not set, use default REDIS_ADDRESS_PARAM")
 	}
-	redisService.HSet(ctx, rds, ConfigKey, "REDIS_ADDRESS_DATA_annotation", "http service get method will fetch data from this redis directly")
+	Cfg.ParamRedis.HSet(ctx, ConfigKey, "REDIS_ADDRESS_DATA_annotation", "http service get method will fetch data from this redis directly")
 
 	REDIS_PASSWORD_DATA := REDIS_PASSWORD_PARAM
-	if err = redisService.HGet(ctx, rds, ConfigKey, "REDIS_PASSWORD_DATA", &REDIS_PASSWORD_DATA); err != nil {
-		redisService.HSet(ctx, rds, ConfigKey, "REDIS_PASSWORD_DATA", REDIS_PASSWORD_PARAM)
+	if cmd := Cfg.ParamRedis.HGet(ctx, ConfigKey, "REDIS_PASSWORD_DATA"); cmd.Err() == nil {
+		REDIS_PASSWORD_DATA = cmd.Val()
+	} else if cmd.Err() != redis.Nil {
+		Cfg.ParamRedis.HSet(ctx, ConfigKey, "REDIS_PASSWORD_DATA", REDIS_PASSWORD_PARAM)
 		fmt.Println("REDIS_PASSWORD_DATA is not set, use default REDIS_PASSWORD_PARAM")
 	}
+
 	REDIS_DB_DATA := REDIS_DB_PARAM
-	if err = redisService.HGet(ctx, rds, ConfigKey, "REDIS_DB_DATA", &REDIS_DB_DATA); err != nil {
-		redisService.HSet(ctx, rds, ConfigKey, "REDIS_DB_DATA", REDIS_DB_PARAM_INT)
+	if cmd := Cfg.ParamRedis.HGet(ctx, ConfigKey, "REDIS_DB_DATA"); cmd.Err() == nil {
+		REDIS_DB_DATA = cmd.Val()
+	} else if cmd.Err() != redis.Nil {
+		Cfg.ParamRedis.HSet(ctx, ConfigKey, "REDIS_DB_DATA", REDIS_DB_PARAM)
 		fmt.Println("REDIS_DB_DATA is not set, use default REDIS_DB_PARAM")
 	}
 	REDIS_DB_DATA_INT, err := strconv.Atoi(REDIS_DB_DATA)
@@ -71,14 +76,19 @@ func LoadConfigFromEnv() (err error) {
 	}
 	Cfg.DataRedis = redis.NewClient(DataRedisOption)
 	//read jwt secret
-	if err = redisService.HGet(ctx, rds, ConfigKey, "JWT_SECRET", &Cfg.JwtSecret); err != nil {
-		redisService.HSet(ctx, rds, ConfigKey, "JWT_SECRET", "")
-		fmt.Println("JWT_SECRET is not set, use default empty string. Restart service after you modify this value")
+	if cmd := Cfg.ParamRedis.HGet(ctx, ConfigKey, "JWT_SECRET"); cmd.Err() == nil {
+		Cfg.JwtSecret = cmd.Val()
+	} else if cmd.Err() != redis.Nil {
+		Cfg.ParamRedis.HSet(ctx, ConfigKey, "JWT_SECRET", "secret")
+		fmt.Println("JWT_SECRET is not set, use default 'secret'")
 	}
 	//read CORS
-	if err = redisService.HGet(ctx, rds, ConfigKey, "CORS", &Cfg.CORS); err != nil {
-		redisService.HSet(ctx, rds, ConfigKey, "CORS", "*")
-		fmt.Println("CORS is not set, use default *")
+	if cmd := Cfg.ParamRedis.HGet(ctx, ConfigKey, "CORS"); cmd.Err() == nil {
+		Cfg.CORS = cmd.Val()
+	} else if cmd.Err() != redis.Nil {
+		Cfg.ParamRedis.HSet(ctx, ConfigKey, "CORS", "*")
+		fmt.Println("CORS is not set, use default '*'")
 	}
+	fmt.Println("Redis configuration loaded. restart service if you configuration is modified")
 	return err
 }
