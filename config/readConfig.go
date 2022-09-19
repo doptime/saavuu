f2d77588f22ad0da9aee045bf31048ada80855f7
yangkequn <yangkequn@gmail.com>
2022-09-19 13:46:13 +0800

    refactor

--- config/readConfig.go
+++ config/readConfig.go
@@ -1,28 +1,35 @@
 package config
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"saavuu/redisService"
 	"strconv"
 
 	"github.com/go-redis/redis/v8"
 )
 
-func LoadConfigFromEnv() {
+func LoadConfigFromEnv() (err error) {
+	ctx := context.Background()
+	os.Setenv("REDIS_ADDRESS_PARAM", "docker.vm:6379")
+	os.Setenv("REDIS_PASSWORD_PARAM", "")
+	os.Setenv("REDIS_DB_PARAM", "0")
+	os.Setenv("REDIS_CONFIG_KEY", "config_saavuu_services")
 	REDIS_ADDRESS_PARAM := os.Getenv("REDIS_ADDRESS_PARAM")
 	if len(REDIS_ADDRESS_PARAM) == 0 {
-		panic("REDIS_ADDRESS_PARAM is not set")
+		panic("env REDIS_ADDRESS_PARAM is not set")
 	}
 
 	REDIS_PASSWORD_PARAM := os.Getenv("REDIS_PASSWORD_PARAM")
 	REDIS_DB_PARAM := os.Getenv("REDIS_DB_PARAM")
 	if len(REDIS_DB_PARAM) == 0 {
-		fmt.Println("REDIS_DB_PARAM is not set, use default 0")
+		fmt.Println("env REDIS_DB_PARAM is not set, use default 0")
 		REDIS_DB_PARAM = "0"
 	}
 	REDIS_DB_PARAM_INT, err := strconv.Atoi(REDIS_DB_PARAM)
 	if err != nil {
-		panic("REDIS_DB_DATA is not a number")
+		panic("env REDIS_DB_DATA is not a number")
 	}
 	ParamRedisOption := &redis.Options{
 		Addr:     REDIS_ADDRESS_PARAM,
@@ -30,21 +37,32 @@ func LoadConfigFromEnv() {
 		DB:       REDIS_DB_PARAM_INT,   // use default DB
 	}
 	Cfg.ParamRedis = redis.NewClient(ParamRedisOption)
-
-	//read redis data config
-	REDIS_ADDRESS_DATA := os.Getenv("REDIS_ADDRESS_DATA")
-	if len(REDIS_ADDRESS_DATA) == 0 {
-		panic("REDIS_ADDRESS_DATA is not set")
+	ConfigKey := os.Getenv("REDIS_CONFIG_KEY")
+	if len(ConfigKey) == 0 {
+		panic("env REDIS_CONFIG_KEY is not set")
 	}
-	REDIS_PASSWORD_DATA := os.Getenv("REDIS_PASSWORD_DATA")
-	REDIS_DB_DATA := os.Getenv("REDIS_DB_DATA")
-	if len(REDIS_DB_DATA) == 0 {
-		fmt.Println("REDIS_DB_DATA is not set, use default 0")
-		REDIS_DB_DATA = "0"
+
+	rds := Cfg.ParamRedis
+	REDIS_ADDRESS_DATA := REDIS_ADDRESS_PARAM
+	if err = redisService.HGet(ctx, rds, ConfigKey, "REDIS_ADDRESS_DATA", &REDIS_ADDRESS_DATA); err != nil {
+		redisService.HSet(ctx, rds, ConfigKey, "REDIS_ADDRESS_DATA", REDIS_ADDRESS_PARAM)
+		fmt.Println("REDIS_ADDRESS_DATA is not set, use default REDIS_ADDRESS_PARAM")
+	}
+	redisService.HSet(ctx, rds, ConfigKey, "REDIS_ADDRESS_DATA_annotation", "http service get method will fetch data from this redis directly")
+
+	REDIS_PASSWORD_DATA := REDIS_PASSWORD_PARAM
+	if err = redisService.HGet(ctx, rds, ConfigKey, "REDIS_PASSWORD_DATA", &REDIS_PASSWORD_DATA); err != nil {
+		redisService.HSet(ctx, rds, ConfigKey, "REDIS_PASSWORD_DATA", REDIS_PASSWORD_PARAM)
+		fmt.Println("REDIS_PASSWORD_DATA is not set, use default REDIS_PASSWORD_PARAM")
+	}
+	REDIS_DB_DATA := REDIS_DB_PARAM
+	if err = redisService.HGet(ctx, rds, ConfigKey, "REDIS_DB_DATA", &REDIS_DB_DATA); err != nil {
+		redisService.HSet(ctx, rds, ConfigKey, "REDIS_DB_DATA", REDIS_DB_PARAM_INT)
+		fmt.Println("REDIS_DB_DATA is not set, use default REDIS_DB_PARAM")
 	}
 	REDIS_DB_DATA_INT, err := strconv.Atoi(REDIS_DB_DATA)
 	if err != nil {
-		panic("REDIS_DB_DATA is not a number")
+		panic("REDIS_DB_DATA is not a number, check redis key: " + ConfigKey + "with field REDIS_DB_DATA  in redis param server. ")
 	}
 	DataRedisOption := &redis.Options{
 		Addr:     REDIS_ADDRESS_DATA,
@@ -52,16 +70,15 @@ func LoadConfigFromEnv() {
 		DB:       REDIS_DB_DATA_INT,   // use default DB
 	}
 	Cfg.DataRedis = redis.NewClient(DataRedisOption)
-
-	if v := os.Getenv("JWT_SECRET"); len(v) > 0 {
-		Cfg.JwtSecret = v
-	} else {
-		panic("JWT_SECRET is not set")
+	//read jwt secret
+	if err = redisService.HGet(ctx, rds, ConfigKey, "JWT_SECRET", &Cfg.JwtSecret); err != nil {
+		redisService.HSet(ctx, rds, ConfigKey, "JWT_SECRET", "")
+		fmt.Println("JWT_SECRET is not set, use default empty string. Restart service after you modify this value")
 	}
-	if v := os.Getenv("MAX_BUFFER_SIZE"); len(v) > 0 {
-		Cfg.MaxBufferSize, _ = strconv.ParseInt(v, 10, 64)
+	//read CORS
+	if err = redisService.HGet(ctx, rds, ConfigKey, "CORS", &Cfg.CORS); err != nil {
+		redisService.HSet(ctx, rds, ConfigKey, "CORS", "*")
+		fmt.Println("CORS is not set, use default *")
 	}
-	if v := os.Getenv("CORS"); len(v) > 0 {
-		Cfg.CORS = v
-	}
+	return err
 }
