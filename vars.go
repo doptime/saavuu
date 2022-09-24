f46554202ce5541ad23d3d03bdd1bdea4e2cda07
yangkequn <yangkequn@gmail.com>
2022-09-24 09:20:30 +0800

    +ErrInvalidInput

--- vars.go
+++ vars.go
@@ -7,3 +7,4 @@ import (
 var ErrJWT error = errors.New("JWT error")
 var ErrParm error = errors.New("parameter error")
 var ErrInvalidData error = errors.New("invalid data")
+var ErrInvalidInput error = errors.New("invalid input")
