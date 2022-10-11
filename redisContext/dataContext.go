3a6c3f7b8e8e8ed8e8f56524114d2059c67b9c25
yangkequn <yangkequn@gmail.com>
2022-10-11 14:50:57 +0800

    add apis

--- redisContext/dataContext.go
+++ redisContext/dataContext.go
@@ -29,26 +29,6 @@ func (dc *DataCtx) Set(key string, param interface{}, expiration time.Duration) 
 	status := dc.Rds.Set(dc.Ctx, key, bytes, expiration)
 	return status.Err()
 }
-func (dc *DataCtx) HGet(key string, field string, param interface{}) (err error) {
-	cmd := dc.Rds.HGet(dc.Ctx, key, field)
-	data, err := cmd.Bytes()
-	if err != nil {
-		return err
-	}
-	return msgpack.Unmarshal(data, param)
-}
-func (dc *DataCtx) HSet(key string, field string, param interface{}) (err error) {
-	bytes, err := msgpack.Marshal(param)
-	if err != nil {
-		return err
-	}
-	status := dc.Rds.HSet(dc.Ctx, key, field, bytes)
-	return status.Err()
-}
-func (dc *DataCtx) HExists(key string, field string) (ok bool) {
-	cmd := dc.Rds.HExists(dc.Ctx, key, field)
-	return cmd.Val()
-}
 
 func (dc *DataCtx) RPush(key string, param interface{}) (err error) {
 	bytes, err := msgpack.Marshal(param)
@@ -99,41 +79,4 @@ func (dc *DataCtx) SMembers(key string, param interface{}) (members []string, er
 		return nil, err
 	}
 	return members, nil
-}
-func (dc *DataCtx) HGetAll(key string, decodeFun func(string) (obj interface{}, erro error)) (param map[string]interface{}, err error) {
-	cmd := dc.Rds.HGetAll(dc.Ctx, key)
-	data, err := cmd.Result()
-	if err != nil {
-		return nil, err
-	}
-	param = make(map[string]interface{})
-	//make a copoy of valueStruct
-	// unmarshal value of data to the copy
-	// store unmarshaled result to param
-	for k, v := range data {
-		if Decoded, err := decodeFun(v); err == nil {
-			param[k] = Decoded
-		} else {
-			return nil, err
-		}
-	}
-	return param, nil
-}
-func (dc *DataCtx) HGetAllDefault(key string) (param map[string]interface{}, err error) {
-	cmd := dc.Rds.HGetAll(dc.Ctx, key)
-	data, err := cmd.Result()
-	if err != nil {
-		return nil, err
-	}
-	param = make(map[string]interface{})
-	//make a copoy of valueStruct
-	// unmarshal value of data to the copy
-	// store unmarshaled result to param
-	for k, v := range data {
-		var obj interface{}
-		if err = msgpack.Unmarshal([]byte(v), &obj); err == nil {
-			param[k] = obj
-		}
-	}
-	return param, nil
 }
