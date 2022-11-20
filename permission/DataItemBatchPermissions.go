ee9423d3934b4f5c8911df1bee0dfd88f5b1d8c2
yangkequn <yangkequn@gmail.com>
2022-11-20 11:13:58 +0800

    remove extra log

--- permission/DataItemBatchPermissions.go
+++ permission/DataItemBatchPermissions.go
@@ -19,13 +19,12 @@ func LoadDataItemBatchPermissionsFromRedis() (err error) {
 		Permissions_TMP map[string]string = map[string]string{}
 		KeyNum          int64             = 0
 	)
-	logger.Lshortfile.Println("start loading DataItemBatchPermissions from redis")
 	// read DataItemBatchPermissions usiing ParamRds
 	// DataItemBatchPermissions is a hash
 	// split each value of DataItemBatchPermissions into string[] and store in PermittedBatchOp
 	Permissions_TMP, err = config.ParamRds.HGetAll(context.Background(), "DataItemBatchPermissions").Result()
-	if err != nil || KeyNum > 0 {
-		logger.Lshortfile.Println("error: " + err.Error() + ". Consider Add hash item  DataItemBatchPermissions in redis,with key redis key before ':' and value as permitted batch operations seperated by ','")
+	if err != nil {
+		logger.Lshortfile.Println("loading DataItemBatchPermissions  error: " + err.Error() + ". Consider Add hash item  DataItemBatchPermissions in redis,with key redis key before ':' and value as permitted batch operations seperated by ','")
 		return err
 	}
 	for k, v := range Permissions_TMP {
@@ -41,7 +40,7 @@ func LoadDataItemBatchPermissionsFromRedis() (err error) {
 	//print info like this: info := fmt.Sprint("loading DataItemBatchPermissions success. num keys:%d PermittedBatchOp size:%d", KeyNum, len(PermittedBatchOp))
 	info := fmt.Sprint("loading DataItemBatchPermissions success. num keys:", KeyNum, " PermittedBatchOp size:", len(PermittedBatchOp))
 	if info != lastLoadDataItemBatchPermissionsInfo {
-		logger.Lshortfile.Println()
+		logger.Lshortfile.Println(info)
 		lastLoadDataItemBatchPermissionsInfo = info
 	}
 	return nil
