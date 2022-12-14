ee44eb2ee149617097af7c6e22ef7fe884f11d0a
yangkequn <yangkequn@gmail.com>
2022-12-14 17:13:05 +0800

    feat: UpdateSchema

--- rCtx/dataContextHash.go
+++ rCtx/dataContextHash.go
@@ -1,6 +1,12 @@
 package rCtx
 
-import "github.com/vmihailenco/msgpack/v5"
+import (
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/vmihailenco/msgpack/v5"
+)
 
 func (dc *DataCtx) HGet(key string, field string, param interface{}) (err error) {
 	cmd := dc.Rds.HGet(dc.Ctx, key, field)
@@ -92,4 +98,88 @@ func (dc *DataCtx) HSetNX(key string, field string, param interface{}) (err erro
 	}
 	status := dc.Rds.HSetNX(dc.Ctx, key, field, bytes)
 	return status.Err()
+}
+
+// golang version of python scan_iter
+func (dc *DataCtx) Scan(match string, cursor uint64, count int64) (keys []string, err error) {
+	keys, _, err = dc.Rds.Scan(dc.Ctx, cursor, match, count).Result()
+	return keys, err
+}
+
+// update redis value schema, the value should be a pointer to  struct of msgpack
+// match is the key pattern, if match end with *, scan all keys start with match
+// example: rename field "V" to "VAL"
+//
+//	demoStruct struct {
+//		//use client's time
+//		Val           int64 `msgpack:"V,alias:VAL"`
+//	}
+func (dc *DataCtx) UpdateSchema(match string, dataStruct interface{}) (err error) {
+	var (
+		val  []byte
+		keys []string = []string{match}
+		data map[string]string
+	)
+	//error check, error if reflect of dataStruct is not a pointer
+	if reflect.TypeOf(dataStruct).Kind() != reflect.Ptr {
+		return fmt.Errorf("dataStruct must be a pointer")
+	}
+	//if keyStart end with *,iter scan all key start with keyStart
+	if strings.HasSuffix(match, "*") {
+		if keys, err = dc.Scan(match, 0, 1024*1024*1024); err != nil {
+			return err
+		}
+	}
+	//check type of redis value
+	for _, key := range keys {
+		if dc.Rds.Type(dc.Ctx, key).Val() == "hash" {
+			cmd := dc.Rds.HGetAll(dc.Ctx, key)
+			if data, err = cmd.Result(); err != nil {
+				return err
+			}
+			pipe := dc.Rds.Pipeline()
+			for field, v := range data {
+				if msgpack.Unmarshal([]byte(v), dataStruct) != nil {
+					return err
+				}
+				bytes, err := msgpack.Marshal(dataStruct)
+				if err != nil {
+					return err
+				}
+				if status := pipe.HSet(dc.Ctx, key, field, bytes); status.Err() != nil {
+					return status.Err()
+				}
+			}
+			if _, err := pipe.Exec(dc.Ctx); err != nil {
+				return err
+			}
+		} else if dc.Rds.Type(dc.Ctx, key).Val() == "string" {
+			if val, err = dc.Rds.Get(dc.Ctx, key).Bytes(); err != nil {
+				return err
+			}
+			if msgpack.Unmarshal(val, dataStruct) != nil {
+				return err
+			}
+			bytes, err := msgpack.Marshal(dataStruct)
+			if err != nil {
+				return err
+			}
+			if err = dc.Rds.Set(dc.Ctx, key, bytes, 0).Err(); err != nil {
+				return err
+			}
+		} else if dc.Rds.Type(dc.Ctx, key).Val() == "list" {
+			//not impleted yet
+			return fmt.Errorf("not impleted yet")
+
+		} else if dc.Rds.Type(dc.Ctx, key).Val() == "set" {
+			//not impleted yet
+			return fmt.Errorf("not impleted yet")
+		} else if dc.Rds.Type(dc.Ctx, key).Val() == "zset" {
+			//not impleted yet
+			return fmt.Errorf("not impleted yet")
+		} else {
+			return fmt.Errorf("unknown type")
+		}
+	}
+	return nil
 }
