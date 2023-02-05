e600292cdbcde5a0b1e370623ab260b09fce2019
yangkequn <yangkequn@gmail.com>
2023-02-05 16:34:00 +0800

    fix :name and spacename colision

--- Rds/ops.go
+++ Rds/ops.go
@@ -11,39 +11,39 @@ import (
 	"github.com/yangkequn/saavuu/logger"
 )
 
-func HSet(ctx context.Context, rds *redis.Client, key string, field string, value interface{}) (err error) {
+func HSet(ctx context.Context, rc *redis.Client, key string, field string, value interface{}) (err error) {
 	bytes, err := msgpack.Marshal(value)
 	if err != nil {
 		return err
 	}
-	status := rds.HSet(ctx, key, field, bytes)
+	status := rc.HSet(ctx, key, field, bytes)
 	return status.Err()
 }
 
-func Get(ctx context.Context, rds *redis.Client, key string, param interface{}) (err error) {
-	cmd := rds.Get(ctx, key)
+func Get(ctx context.Context, rc *redis.Client, key string, param interface{}) (err error) {
+	cmd := rc.Get(ctx, key)
 	data, err := cmd.Bytes()
 	if err != nil {
 		return err
 	}
 	return msgpack.Unmarshal(data, param)
 }
-func Set(ctx context.Context, rds *redis.Client, key string, param interface{}, expiration time.Duration) (err error) {
+func Set(ctx context.Context, rc *redis.Client, key string, param interface{}, expiration time.Duration) (err error) {
 	bytes, err := msgpack.Marshal(param)
 	if err != nil {
 		return err
 	}
-	status := rds.Set(ctx, key, bytes, expiration)
+	status := rc.Set(ctx, key, bytes, expiration)
 	return status.Err()
 }
 
-func HGetAll(ctx context.Context, rds *redis.Client, key string, mapOut interface{}) (err error) {
+func HGetAll(ctx context.Context, rc *redis.Client, key string, mapOut interface{}) (err error) {
 	mapElem := reflect.TypeOf(mapOut)
 	if (mapElem.Kind() != reflect.Map) || (mapElem.Key().Kind() != reflect.String) {
 		logger.Lshortfile.Println("mapOut must be a map[string] struct/interface{}")
 		return errors.New("mapOut must be a map[string] struct/interface{}")
 	}
-	cmd := rds.HGetAll(ctx, key)
+	cmd := rc.HGetAll(ctx, key)
 	data, err := cmd.Result()
 	if err != nil {
 		return err
@@ -60,68 +60,68 @@ func HGetAll(ctx context.Context, rds *redis.Client, key string, mapOut interfac
 	return err
 }
 
-func HKeys(ctx context.Context, rds *redis.Client, key string) (fields []string, err error) {
-	cmd := rds.HKeys(ctx, key)
+func HKeys(ctx context.Context, rc *redis.Client, key string) (fields []string, err error) {
+	cmd := rc.HKeys(ctx, key)
 	return cmd.Val(), cmd.Err()
 }
 
-func RPush(ctx context.Context, rds *redis.Client, key string, param interface{}) (err error) {
+func RPush(ctx context.Context, rc *redis.Client, key string, param interface{}) (err error) {
 	bytes, err := msgpack.Marshal(param)
 	if err != nil {
 		return err
 	}
-	status := rds.RPush(ctx, key, bytes)
+	status := rc.RPush(ctx, key, bytes)
 	return status.Err()
 }
-func LSet(ctx context.Context, rds *redis.Client, key string, index int64, param interface{}) (err error) {
+func LSet(ctx context.Context, rc *redis.Client, key string, index int64, param interface{}) (err error) {
 	bytes, err := msgpack.Marshal(param)
 	if err != nil {
 		return err
 	}
-	status := rds.LSet(ctx, key, index, bytes)
+	status := rc.LSet(ctx, key, index, bytes)
 	return status.Err()
 }
-func LGet(ctx context.Context, rds *redis.Client, key string, index int64, param interface{}) (err error) {
-	cmd := rds.LIndex(ctx, key, index)
+func LGet(ctx context.Context, rc *redis.Client, key string, index int64, param interface{}) (err error) {
+	cmd := rc.LIndex(ctx, key, index)
 	data, err := cmd.Bytes()
 	if err != nil {
 		return err
 	}
 	return msgpack.Unmarshal(data, param)
 }
-func LLen(ctx context.Context, rds *redis.Client, key string) (length int64, err error) {
-	cmd := rds.LLen(ctx, key)
+func LLen(ctx context.Context, rc *redis.Client, key string) (length int64, err error) {
+	cmd := rc.LLen(ctx, key)
 	return cmd.Result()
 }
-func SAdd(ctx context.Context, rds *redis.Client, key string, param interface{}) (err error) {
+func SAdd(ctx context.Context, rc *redis.Client, key string, param interface{}) (err error) {
 	bytes, err := msgpack.Marshal(param)
 	if err != nil {
 		return err
 	}
-	status := rds.SAdd(ctx, key, bytes)
+	status := rc.SAdd(ctx, key, bytes)
 	return status.Err()
 }
-func SRem(ctx context.Context, rds *redis.Client, key string, param interface{}) (err error) {
+func SRem(ctx context.Context, rc *redis.Client, key string, param interface{}) (err error) {
 	bytes, err := msgpack.Marshal(param)
 	if err != nil {
 		return err
 	}
-	status := rds.SRem(ctx, key, bytes)
+	status := rc.SRem(ctx, key, bytes)
 	return status.Err()
 }
-func SIsMember(ctx context.Context, rds *redis.Client, key string, param interface{}) (isMember bool, err error) {
+func SIsMember(ctx context.Context, rc *redis.Client, key string, param interface{}) (isMember bool, err error) {
 	bytes, err := msgpack.Marshal(param)
 	if err != nil {
 		return false, err
 	}
-	cmd := rds.SIsMember(ctx, key, bytes)
+	cmd := rc.SIsMember(ctx, key, bytes)
 	return cmd.Result()
 }
-func SMembers(ctx context.Context, rds *redis.Client, key string) (members []string, err error) {
-	cmd := rds.SMembers(ctx, key)
+func SMembers(ctx context.Context, rc *redis.Client, key string) (members []string, err error) {
+	cmd := rc.SMembers(ctx, key)
 	return cmd.Result()
 }
-func Time(ctx context.Context, rds *redis.Client) (time time.Time, err error) {
-	cmd := rds.Time(ctx)
+func Time(ctx context.Context, rc *redis.Client) (time time.Time, err error) {
+	cmd := rc.Time(ctx)
 	return cmd.Result()
 }
