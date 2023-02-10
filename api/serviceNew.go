05956b2fb237a41cf8371b65830d8dac061b5df7
yangkequn <yangkequn@gmail.com>
2023-02-10 11:22:31 +0800

    refactor NewApi: to avoid early DataRds initial

--- api/serviceNew.go
+++ api/serviceNew.go
@@ -16,26 +16,27 @@ type fn func(dc *data.Ctx, pc *Ctx, paramIn map[string]interface{}) (out map[str
 var ErrBackTo = errors.New("param[\"backTo\"] is not a string")
 
 func NewApi(serviceName string, f fn) {
-	serviceName = "api:" + serviceName
-	//check configureation is loaded
-	if config.DataRds == nil {
-		logger.Lshortfile.Panic("config.DataRedis is nil. you should call config.LoadConfigFromRedis first")
-	}
-	if config.ParamRds == nil {
-		logger.Lshortfile.Panic("config.ParamRedis is nil. you should call config.LoadConfigFromRedis first")
-	}
 	ProcessOneJob := func(BackToID string, s []byte) (err error) {
 		var (
 			out            interface{}
 			marshaledBytes []byte
 			param          map[string]interface{} = map[string]interface{}{}
+			dc             *data.Ctx
+			pc             *Ctx
 		)
 		if err = msgpack.Unmarshal(s, &param); err != nil {
 			return err
 		}
 		//process one job
-		dc := &data.Ctx{Ctx: context.Background(), Rds: config.DataRds}
-		pc := &Ctx{Ctx: context.Background(), Rds: config.ParamRds}
+		//check configureation is loaded
+		if config.DataRds == nil {
+			logger.Lshortfile.Panic("config.DataRedis is nil. Call config.ApiInitial first")
+		}
+		if config.ParamRds == nil {
+			logger.Lshortfile.Panic("config.ParamRedis is nil. Call config.ApiInitial first")
+		}
+		dc = &data.Ctx{Ctx: context.Background(), Rds: config.DataRds}
+		pc = &Ctx{Ctx: context.Background(), Rds: config.ParamRds}
 		if out, err = f(dc, pc, param); err != nil {
 			return err
 		}
@@ -50,6 +51,7 @@ func NewApi(serviceName string, f fn) {
 		_, err = pipline.Exec(ctx)
 		return err
 	}
+	serviceName = "api:" + serviceName
 	apiServices[serviceName] = &ApiInfo{
 		ApiName: serviceName,
 		ApiFunc: ProcessOneJob,
