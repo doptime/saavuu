eebe467d50802b167983773a93f2c20e815b6dd3
yangkequn <yangkequn@gmail.com>
2023-03-05 11:17:56 +0800

    refactor redis key to type RedisKey

--- key/key.go
+++ key/key.go
@@ -5,27 +5,29 @@ import (
 	"time"
 )
 
-func YMD(Key string, tm time.Time) string {
+type RedisKey string
+
+func (Key RedisKey) YMD(tm time.Time) string {
 	//year is 4 digits, month is 2 digits, day is 2 digits
 	return fmt.Sprintf("%sYMD:%04v%02v%02v", Key, tm.Year(), int(tm.Month()), tm.Day())
 }
-func YM(Key string, tm time.Time) string {
+func (Key RedisKey) YM(tm time.Time) string {
 	//year is 4 digits, month is 2 digits
 	return fmt.Sprintf("%sYM:%04v%02v", Key, tm.Year(), int(tm.Month()))
 }
-func Y(Key string, tm time.Time) string {
+func (Key RedisKey) Y(tm time.Time) string {
 	//year is 4 digits
 	return fmt.Sprintf("%sY:%04v", Key, tm.Year())
 }
-func YW(Key string, tm time.Time) string {
+func (Key RedisKey) YW(tm time.Time) string {
 	tm = tm.UTC()
 	isoYear, isoWeek := tm.ISOWeek()
 	//year is 4 digits, week is 2 digits
 	return fmt.Sprintf("%sYW:%04v%02v", Key, isoYear, isoWeek)
 }
-func MultiPart(Key string, fields ...interface{}) string {
+func (Key RedisKey) Concat(fields ...interface{}) string {
 	//	concacate all fields with ':'
-	strAll := Key
+	strAll := string(Key)
 	for _, field := range fields {
 		//convert field to string,field may be int, float, string, etc.
 		strAll += fmt.Sprintf(":%v", field)
