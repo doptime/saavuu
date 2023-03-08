fd5ac1389f734d46be656c7ab8b1d81933785eaa
yangkequn <yangkequn@gmail.com>
2023-03-08 11:27:00 +0800

    fix ErrBadCommand

--- https/del.go
+++ https/del.go
@@ -16,27 +16,24 @@ func (svcCtx *HttpContext) DelHandler() (result interface{}, err error) {
 	switch svcCtx.Cmd {
 	case "HDEL":
 		//error if empty Key or Field
-		if svcCtx.Key == "" || svcCtx.Field == "" {
+		if svcCtx.Field == "" {
 			return "false", ErrEmptyKeyOrField
 		}
 		if !permission.IsDelPermitted(svcCtx.Key, "hdel") {
 			return "false", errors.New("permission denied")
 		}
 		cmd := config.DataRds.HDel(svcCtx.Ctx, svcCtx.Key, svcCtx.Field)
-		if err = cmd.Err(); err != nil {
-			return "false", err
+		if err = cmd.Err(); err == nil {
+			return "true", nil
 		}
-		return "true", nil
 	case "DEL":
-		//error if empty Key or Field
-		if svcCtx.Key == "" {
-			return "false", ErrEmptyKeyOrField
-		}
 		cmd := config.ParamRds.HDel(svcCtx.Ctx, svcCtx.Key, "del")
-		if err = cmd.Err(); err != nil {
-			return nil, err
+		if err = cmd.Err(); err == nil {
+			return "true", nil
 		}
+	default:
+		return nil, ErrBadCommand
 	}
 
-	return "true", nil
+	return "false", err
 }
