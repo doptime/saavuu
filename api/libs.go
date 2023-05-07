816e16feea5563cb0e8c24798e824f7f0d516d72
yangkequn <yangkequn@gmail.com>
2023-05-07 10:04:28 +0800

    fix ApiLockKey to prevent dueTime refresh

--- api/libs.go
+++ api/libs.go
@@ -1,11 +1,12 @@
 package api
 
 import (
+	"context"
 	"strconv"
 	"time"
 
 	"github.com/redis/go-redis/v9"
-	"github.com/yangkequn/saavuu/data"
+	"github.com/yangkequn/saavuu/config"
 )
 
 type InLockKey struct {
@@ -13,27 +14,34 @@ type InLockKey struct {
 	DurationMs int64
 }
 
-var keyTimeLocker = data.New[bool]("TimeLocker")
 var removeCounter int64 = 90
 
 var ApiLockKey = Api(func(req *InLockKey) (ok bool, err error) {
-
 	var (
 		now     int64 = time.Now().UnixMicro()
 		dueTime int64 = now + req.DurationMs
 		score   float64
 	)
-	ok = true
-	if score, err = keyTimeLocker.ZScore(req.Key); err == nil && score > float64(now) {
-		ok = false
+	ok = false
+	if score, err = config.Rds.ZScore(context.Background(), "KeyLocker", req.Key).Result(); err != nil {
+		if err != redis.Nil {
+			return false, err
+		} else {
+			ok = true
+		}
+	} else if score < float64(now) {
+		ok = true
 	}
-	keyTimeLocker.ZAdd(redis.Z{Score: float64(dueTime), Member: req.Key})
+	//update only when key not exists, or expired
+	if ok {
+		config.Rds.ZAdd(context.Background(), "KeyLocker", redis.Z{Score: float64(dueTime), Member: req.Key})
+	}
 
 	//auto remove expired keys
 	removeCounter += 1
 	if removeCounter > 100 {
 		removeCounter = 0
-		go keyTimeLocker.ZRemRangeByScore("0", strconv.FormatInt(now, 10))
+		go config.Rds.ZRemRangeByScore(context.Background(), "KeyLocker", "0", strconv.FormatInt(now, 10))
 	}
 	return ok, nil
 })
