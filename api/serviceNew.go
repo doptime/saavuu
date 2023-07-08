26101dd2c46ce0af28ba567cf55275501a9de32a
yangkequn <yangkequn@gmail.com>
2023-07-08 12:35:14 +0800

    refactor: more readable

--- api/serviceNew.go
+++ api/serviceNew.go
@@ -13,20 +13,19 @@ var ErrBackTo = errors.New("param[\"backTo\"] is not a string")
 
 // Key purpose of ApiNamed is to allow different API to have the same input type
 func ApiNamed[i any, o any](ServiceName string, f func(InParameter i) (ret o, err error)) (retf func(InParam i) (ret o, err error), ctx *Ctx[i, o]) {
-	// JWT info is stored in Field MsgPack.
-	type InParamWithMsgPack struct {
-		OriginalInputParam i
-		MsgPack            []byte
-	}
 	//create Api context
 	//Serivce name should Start with "api:"
 	ctx = New[i, o](ServiceName)
 	ctx.Func = f
 	//create a goroutine to process one job
 	ProcessOneJob := func(s []byte) (ret interface{}, err error) {
+		type JWTInfoMsgpacked struct {
+			MsgPack []byte
+		}
 		var (
-			out   o
-			param InParamWithMsgPack
+			in  i
+			out o
+			jwt JWTInfoMsgpacked
 		)
 		//check configureation is loaded
 		if config.Rds == nil {
@@ -34,22 +33,22 @@ func ApiNamed[i any, o any](ServiceName string, f func(InParameter i) (ret o, er
 		}
 
 		//step 1, try to unmarshal MsgPack
-		err = msgpack.Unmarshal(s, &param)
+		err = msgpack.Unmarshal(s, &jwt)
 
 		// case double pointer decoding
 		if vType := reflect.TypeOf((*i)(nil)).Elem(); err == nil && vType.Kind() == reflect.Ptr {
 			//step 2, try to unmarshal OriginalInputParam
-			err = msgpack.Unmarshal(s, param.OriginalInputParam)
-			if err == nil && len(param.MsgPack) > 0 {
+			err = msgpack.Unmarshal(s, in)
+			if err == nil && len(jwt.MsgPack) > 0 {
 				//step 3, try to unmarshal MsgPack
-				err = msgpack.Unmarshal(param.MsgPack, param.OriginalInputParam)
+				err = msgpack.Unmarshal(jwt.MsgPack, in)
 			}
 		} else if err == nil {
 			//step 2, try to unmarshal OriginalInputParam
-			err = msgpack.Unmarshal(s, &param.OriginalInputParam)
-			if err == nil && len(param.MsgPack) > 0 {
+			err = msgpack.Unmarshal(s, &in)
+			if err == nil && len(jwt.MsgPack) > 0 {
 				//step 3, try to unmarshal MsgPackƒ
-				err = msgpack.Unmarshal(param.MsgPack, &param.OriginalInputParam)
+				err = msgpack.Unmarshal(jwt.MsgPack, &in)
 			}
 		}
 		//print the unmarshal error
@@ -59,7 +58,7 @@ func ApiNamed[i any, o any](ServiceName string, f func(InParameter i) (ret o, er
 			}
 			return nil, err
 		}
-		if out, err = f(param.OriginalInputParam); err != nil {
+		if out, err = f(in); err != nil {
 			return nil, err
 		}
 
