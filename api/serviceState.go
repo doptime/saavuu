cddbd6bd9c5288add564ce3b4d22d6b2933ae5f8
yangkequn <yangkequn@gmail.com>
2023-10-17 21:48:41 +0800

    allow refresh serviceNames

--- api/serviceState.go
+++ api/serviceState.go
@@ -19,6 +19,7 @@ func reportStates() {
 	for {
 		time.Sleep(time.Second * 60)
 		now := time.Now().String()[11:19]
+		serviceNames = apiServiceNames()
 		for _, serviceName := range serviceNames {
 			if num, _ := apiCounter.Get(serviceName); num > 0 {
 				log.Info().Msg(now + "" + serviceName + " proccessed " + strconv.Itoa(int(num)) + " tasks")
