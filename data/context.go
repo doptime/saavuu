9666b037dce31927fc259d317ddc37f371bfc6cf
yangkequn <yangkequn@gmail.com>
2023-10-18 15:13:36 +0800

    add log for Data context create

--- data/context.go
+++ data/context.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog/log"
 	"github.com/yangkequn/saavuu/config"
 )
 
@@ -23,19 +24,29 @@ func NewStruct[k comparable, v any]() *Ctx[k, v] {
 		_type = _type.Elem()
 	}
 	Key = _type.Name()
+
+	log.Debug().Str("data NewStruct create start!", Key).Send()
+
 	//panic if Key is empty
 	if Key == "" {
 		panic("Key is empty, please give a key for this data")
 	}
-	return &Ctx[k, v]{Ctx: context.Background(), Rds: config.Rds, Key: Key}
+	ctx := &Ctx[k, v]{Ctx: context.Background(), Rds: config.Rds, Key: Key}
+
+	log.Debug().Str("data NewStruct create end!", Key).Send()
+
+	return ctx
 }
 
 func New[k comparable, v any](Key string) *Ctx[k, v] {
+	log.Debug().Str("data New create start!", Key).Send()
 	//panic if Key is empty
 	if Key == "" {
 		panic("Key is empty, please give a key for this data")
 	}
-	return &Ctx[k, v]{Ctx: context.Background(), Rds: config.Rds, Key: Key}
+	ctx := &Ctx[k, v]{Ctx: context.Background(), Rds: config.Rds, Key: Key}
+	log.Debug().Str("data New create end!", Key).Send()
+	return ctx
 }
 func (ctx *Ctx[k, v]) WithContext(c context.Context) *Ctx[k, v] {
 	return &Ctx[k, v]{Ctx: c, Rds: ctx.Rds, Key: ctx.Key}
