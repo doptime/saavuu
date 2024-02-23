2c22e93bb193c33f88013ff911e9c0bb0871bf25
yangkequn <yangkequn@gmail.com>
2024-02-23 12:00:29 +0800

    fix :CallByHTTP use the api data source defined in apiInfo

--- api/apiCallByHttp.go
+++ api/apiCallByHttp.go
@@ -8,22 +8,22 @@ import (
 
 func CallByHTTP(ServiceName string, paramIn map[string]interface{}) (ret interface{}, err error) {
 	var (
-		fuc *ApiInfo
-		ok  bool
-		buf []byte
+		apiInfo *ApiInfo
+		ok      bool
+		buf     []byte
 	)
 	if ServiceName = specification.ApiName(ServiceName); len(ServiceName) == 0 {
 		return nil, fmt.Errorf("service misnamed %s", ServiceName)
 	}
 	//if function is stored locally, call it directly. This is alias monolithic mode
-	if fuc, ok = ApiServices.Get(ServiceName); !ok {
+	if apiInfo, ok = ApiServices.Get(ServiceName); !ok {
 		//if function is not stored locally, call it remotely (RPC). This is alias microservice mode
-		var rpc = Rpc[interface{}, interface{}](OptName(ServiceName))
+		var rpc = Rpc[interface{}, interface{}](OptName(ServiceName), OptDb(apiInfo.DbName))
 		return rpc(paramIn)
 	}
 	//if function is stored locally, call it directly. This is alias monolithic mode
 	if buf, err = specification.MarshalApiInput(paramIn); err != nil {
 		return nil, err
 	}
-	return fuc.ApiFuncWithMsgpackedParam(buf)
+	return apiInfo.ApiFuncWithMsgpackedParam(buf)
 }
